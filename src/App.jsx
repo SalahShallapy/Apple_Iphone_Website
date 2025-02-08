d231469b62ba14d034e450f368a6f81f9e924da2
shalapy <salahshalapy999@gmail.com>
2025-02-08 17:08:52 +0200

    Updated app component adding the carousel and model

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Highlights from "./components/Highlights";
 import NavBar from "./components/NavBar";
 import HeroSection from "./components/HeroSection.jsx";
+import Model from "./components/Model.jsx";
 const App = () => {
   return (
     <main className="bg-black">
       <NavBar />
       <HeroSection />
       <Highlights />
+      <Model />
     </main>
   );
 };

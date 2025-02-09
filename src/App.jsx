42169fe580b10e67a4501b4a6f8d3f3d0e410198
shalapy <salahshalapy999@gmail.com>
2025-02-09 17:29:11 +0200

    Added Sentry wrappper and plugin for tracking optimization plus 3 components rendered

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import Highlights from "./components/Highlights";
 import NavBar from "./components/NavBar";
 import HeroSection from "./components/HeroSection.jsx";
 import Model from "./components/Model.jsx";
+import Features from "./components/Features.jsx";
+import Chip from "./components/Chip.jsx";
+import Footer from "./components/Footer.jsx";
+import * as Sentry from "@sentry/react";
+
 const App = () => {
   return (
     <main className="bg-black">
@@ -9,8 +14,11 @@ const App = () => {
       <HeroSection />
       <Highlights />
       <Model />
+      <Features />
+      <Chip />
+      <Footer />
     </main>
   );
 };
 
-export default App;
+export default Sentry.withProfiler(App);

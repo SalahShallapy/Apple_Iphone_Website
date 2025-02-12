5dc6677640ee3753f9c07315cfe73376293afb9b
shalapy <salahshalapy999@gmail.com>
2025-02-12 23:40:33 +0200

    Modified navbar styles and sizes in pc view port repecting respnsivness

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,29 @@
 import { appleImg, bagImg, searchImg } from "../utils";
 import { navLists } from "../constants";
+
 const NavBar = () => {
   return (
-    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+    <header className="w-full py-4 px-8 flex justify-between items-center">
       <nav className="flex w-full screen-max-width">
-        <img src={appleImg} alt="Apple" width={14} height={18} />
+        {/* Apple Logo */}
+        <img src={appleImg} alt="Apple" width={18} height={22} />
 
-        <div className="flex flex-1 justify-center max-sm:hidden">
+        {/* Nav Links - Hidden on Small Screens */}
+        <div className="flex flex-1 justify-center gap-10 max-sm:hidden">
           {navLists.map((nav) => (
             <div
               key={nav}
-              className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all"
+              className="px-5 text-base cursor-pointer text-gray-400 hover:text-white transition duration-300"
             >
               {nav}
             </div>
           ))}
         </div>
 
-        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
-          <img src={searchImg} alt="search" width={18} height={18} />
-          <img src={bagImg} alt="sbag" width={18} height={18} />
+        {/* Right Icons */}
+        <div className="flex items-center gap-8 max-sm:justify-end max-sm:flex-1">
+          <img src={searchImg} alt="search" width={22} height={22} />
+          <img src={bagImg} alt="bag" width={22} height={22} />
         </div>
       </nav>
     </header>
